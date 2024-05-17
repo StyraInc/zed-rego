c9c43cd2aac2d3b8f5743aa877bdf8027a49d9a7
sspaink <sebastianspaink@gmail.com>
2024-05-17 17:45:29 -0500

    download regal

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,104 @@
+use std::fs;
+
 use zed_extension_api::{self as zed, Result};
 
-struct MyExtension {
-    // ... state
+struct Rego {
+    cached_binary_path: Option<String>,
 }
 
-impl zed::Extension for MyExtension {
+impl Rego {
+    fn language_server_binary(
+        &mut self,
+        language_server_id: &zed::LanguageServerId,
+        worktree: &zed::Worktree,
+    ) -> Result<String>  {
+        if let Some(path) = worktree.which("regal") {
+            return Ok(path);
+        }
+
+        if let Some(path) = &self.cached_binary_path {
+            if fs::metadata(path).map_or(false, |stat| stat.is_file()) {
+                return Ok(path.clone());
+            }
+        }
+
+        zed::set_language_server_installation_status(
+            &language_server_id,
+            &zed::LanguageServerInstallationStatus::CheckingForUpdate,
+        );
+
+        let release = zed::latest_github_release(
+            "StyraInc/regal",
+            zed::GithubReleaseOptions {
+                require_assets: true,
+                pre_release: false,
+            },
+        )?;
+
+        let version_dir = format!("rego-{}", release.version);
+        //let binary_path = format!("{version_dir}/bin/rego");
+        let (platform, arch) = zed::current_platform();
+        let asset_name = format!(
+            "regal_{os}_{arch}{extension}",
+            os = match platform {
+                zed::Os::Mac => "Darwin",
+                zed::Os::Linux => "Linux",
+                zed::Os::Windows => "Windows",
+            },
+            arch = match arch {
+                zed::Architecture::Aarch64 => "arm64",
+                zed::Architecture::X86 => "x86",
+                zed::Architecture::X8664 => "x86_64",
+            },
+            extension = match platform {
+                zed::Os::Mac | zed::Os::Linux => "",
+                zed::Os::Windows => ".exe",
+            }
+        );
+
+        let binary_path = format!("{version_dir}/{asset_name}");
+
+        if !fs::metadata(&binary_path).map_or(false, |stat| stat.is_file()) {
+            zed::set_language_server_installation_status(
+                &language_server_id,
+                &zed::LanguageServerInstallationStatus::Downloading,
+            );
+    
+            let asset = release
+            .assets
+            .iter()
+            .find(|asset| asset.name == asset_name)
+            .ok_or_else(|| format!("no asset found matching {:?}", asset_name))?;
+
+            zed::download_file(
+                &asset.download_url,
+                &binary_path,
+                zed::DownloadedFileType::Uncompressed,
+            )
+            .map_err(|e| format!("failed to download file: {e}"))?;
+
+            zed::make_file_executable(&binary_path)?;
+
+            let entries =
+                fs::read_dir(".").map_err(|e| format!("failed to list working directory {e}"))?;
+            for entry in entries {
+                let entry = entry.map_err(|e| format!("failed to load directory entry {e}"))?;
+                if entry.file_name().to_str() != Some(&version_dir) {
+                    fs::remove_dir_all(&entry.path()).ok();
+                }
+            }
+        }
+
+        self.cached_binary_path = Some(binary_path.clone());
+        Ok(binary_path)
+    }
+}
+
+impl zed::Extension for Rego {
     fn new() -> Self {
-        Self {}
+        Self {
+            cached_binary_path: None,
+        }
     }
 
     fn language_server_command(
@@ -14,8 +106,12 @@ impl zed::Extension for MyExtension {
         language_server_id: &zed::LanguageServerId,
         worktree: &zed::Worktree,
     ) -> Result<zed::Command> {
-        Err("Hello World! I am not implemented".into())
+        Ok(zed::Command {
+            command: self.language_server_binary(language_server_id, worktree)?,
+            args: vec!["language-server".to_string()],
+            env: Default::default(),
+        })
     }
 }
 
-zed::register_extension!(MyExtension);
+zed::register_extension!(Rego);
